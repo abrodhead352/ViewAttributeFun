7d99613837a11c6c1b6daf98656f65b79e4d84ec
Brodhead <abrodhead@zagmail.gonzaga.edu>
2019-10-03 10:28:12 -0700

    initial commit

---
+++ app/src/main/java/com/example/viewattributefun/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.viewattributefun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button b = (Button) findViewById(R.id.buttonOn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(b.getText().toString().equals("on")){
+                    b.setText("off");
+                } else {
+                    b.setText("on");
+                }
+            }
+        });
+    }
+}
